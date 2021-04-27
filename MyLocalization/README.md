275990f3abab946fc263afa3c5dec48a6a8680e9
yagysumule <codewithdiyah@gmail.com>
2021-04-27 15:26:51 +0700

    Initial commit

---
+++ MyLocalization/README.md
@@ -0,0 +1,8 @@
+# Tujuan
+Pada Codelab kali ini kita akan membuat aplikasi yang mendukung banyak Bahasa. Codelab kali ini dimaksudkan untuk memberi pengetahuan tentang cara menerapkan localization ke dalam sebuah aplikasi. Hasil dari codelab ini akan menjadi seperti ini:
+
+![MyLocalization](https://user-images.githubusercontent.com/68750843/116207905-8d10ea00-a76a-11eb-9318-43f117bfe762.gif)
+
+
+# Logika Dasar
+Membuka Aplikasi → mengubah bahasa → bahasa pada Aplikasi akan berubah.
---
+++ MyLocalization/app/src/main/java/com/yagysumule/mylocalization/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.yagysumule.mylocalization
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.yagysumule.mylocalization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pokeCount = 3
+        val hello = resources.getString(R.string.hello_world, "Narenda Wicaksono", pokeCount, "Yoza Aprilio")
+
+        binding.tvHello.text = hello
+
+        val songCount = 5
+        val pluralText = resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCount, songCount)
+        binding.tvPlural.text = pluralText
+
+        binding.tvXliff.text = resources.getString(R.string.app_homeurl)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
